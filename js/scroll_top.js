cf72105a44d95c5322c5817e9c2f55d38b2f4b16
riku0329 user name <riku03290@gmail.com>
2020-06-15 21:38:02 +0900

    init

---
+++ js/scroll_top.js
@@ -0,0 +1,30 @@
+// DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
+$(function () {
+  // トップへ戻るボタンのエレメントを取得
+  const scrollTopEl = $('.js-scroll-top');
+  // ボタンをクリックしたらページトップまで戻るを実装
+  scrollTopEl.on('click', function () {
+    // スクロールの速度(ミリ秒)
+    const speed = 300;
+    // スムーススクロールを実行
+    $('body,html').animate({ scrollTop: 0 }, speed, 'swing');
+    return false;
+  });
+
+  // windowのスクロール位置でボタンの表示・非表示
+  $(window).on('scroll', function () {
+    // スクロールの速度(ミリ秒)
+    const speed = 500;
+    // 現在のスクロール位置を取得(windowのscrollTopを取得)
+    const currentPosition = window.scrollY;
+    console.log(currentPosition);
+    // もし現在のスクロール位置が30を超えていたらトップへ戻るボタンのエレメントをfadeInで表示
+    if (currentPosition >= 30) {
+      $('.js-scroll-top').css('display', 'flex');
+    }
+    // 30を超えていない場合はfadeOutで非表示
+    if (currentPosition <= 30) {
+      $('.js-scroll-top').css('display', 'none');
+    }
+  });
+});
---
+++ js/smooth_scroll.js
@@ -0,0 +1,25 @@
+// DOMツリーが構築されたときに実行(他のJavascriptの読込も完了した後に実行します)
+$(function () {
+  // #から始まるリンク(内部リンク)のクリックイベントに実装
+  $('[href^="#"]').on('click', function (event) {
+    // デフォルトの動作をキャンセル
+    event.preventDefault();
+    // スクロールの速度(ミリ秒)
+    const speed = 300;
+    // アンカー(href)の値取得
+    const href = $(this).attr('href');
+    console.log(`href: ${href}`);
+    // 移動先を取得(リンク先のidが存在するかを確認してトップに戻るかどうかを切り替える)
+    const targetId = $(href === '#' || href === '' ? 'html' : href);
+    console.log(`targetId: ${targetId}`);
+    // 移動先を数値で取得(エレメントの上端の位置を取得)
+    const position = targetId.offset().top;
+    console.log(`position: ${position}`);
+    // 固定のヘッダの高さを取得(隙間を一文字分足しておく)
+    const offset = $('.navbar').outerHeight() + 13
+    console.log(`offset: ${offset}`);
+    // スムーススクロールを実行
+    $('body,html').animate({ scrollTop: position - offset }, speed, 'swing');
+    return false;
+  });
+});
